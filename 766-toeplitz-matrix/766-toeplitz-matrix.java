4cd334df5ca59e2ca0ed12ddd9db54eb8776712e
Pranjal Jain <96062187+jainpranjal028@users.noreply.github.com>
2022-01-06 18:28:55 +0530

    Time: 1 ms (77.48%), Space: 42.6 MB (24.17%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.java
+++ 766-toeplitz-matrix/766-toeplitz-matrix.java
@@ -1,19 +1,23 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
         
-        for(int i =0 ; i<matrix.length-1; i++)
+        if(matrix.length == 1 && matrix[0].length == 1)
         {
-            for(int j =0; j<matrix[i].length-1; j++)
+               return true;
+        }
+        
+        int lengthR = matrix.length - 1, lengthC = matrix[0].length - 1;
+         
+        for(int i =0 ; i<lengthR; i++)
+        {
+            for(int j =0; j<lengthC; j++)
             {
                 if(matrix[i][j] != matrix[i+1][j+1])
                 {
                     return false;
-                }
-                
+                } 
             }
         }
         return true;
-        
-        
     }
 }
