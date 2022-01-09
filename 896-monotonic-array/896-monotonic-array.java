976e1f50c53e20bbbd620cd7f0d7ac67bb56f3c4
Pranjal Jain <96062187+jainpranjal028@users.noreply.github.com>
2022-01-09 23:10:03 +0530

    Time: 5 ms (13.85%), Space: 114.6 MB (17.95%) - LeetHub

---
+++ 896-monotonic-array/896-monotonic-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        int inc = 0 ,dec =0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i]==nums[i+1]){
+                continue;
+            }else if(nums[i]<nums[i+1]){
+                inc++;
+            }else{
+                dec++;
+            }
+        }
+        if(inc>0 && dec>0)return false;
+        
+        return true;
+    }
+}
