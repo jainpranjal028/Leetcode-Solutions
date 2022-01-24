d33bc83602beae08dba649aa2c6cda62e1746bd0
Pranjal Jain <96062187+jainpranjal028@users.noreply.github.com>
2022-01-24 20:31:52 +0530

    Time: 5 ms (13.83%), Space: 49 MB (25.36%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -1,56 +1,22 @@
 class Solution {
     public int numSpecial(int[][] mat) {
+                int[] row = new int[mat.length];
+        int[] col = new int[mat[0].length];
         
-      int[] rowmatrix = new int[mat.length];
-      int[] columnmatrix = new int[mat[0].length];
-        
-       for(int i =0; i<mat.length; i++)
-       {
-           int count=0;
-           for(int j =0; j<mat[i].length; j++)
-           {
-               if(mat[i][j]==1)
-               {
-                   count++;
-               }
-               
-           }
-           rowmatrix[i] = count;
-       }
-        
-        for(int j =0; j<mat[0].length; j++)
-       {
-           int count=0;
-           for(int i =0; i<mat.length; i++)
-           {
-               if(mat[i][j]==1)
-               {
-                   count++;
-               }
-               
-           }
-           columnmatrix[j] = count;
-       }
-        
-       //---------------------------------------------------------------
-        
-        int spos=0;
-        for(int i =0; i<mat.length; i++)
-        {
-            for(int j=0; j<mat[i].length; j++)
-            {
-                if(mat[i][j]==1  && rowmatrix[i]==1 && columnmatrix[j]==1)
-                {
-                    spos++;
-                }
-               
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0; j < mat[i].length; j++) {
+                row[i] += mat[i][j];
+                col[j] += mat[i][j];
             }
         }
         
-        return spos;
+        int count = 0;
+          for(int i = 0; i < mat.length; i++) {
+            for(int j = 0; j < mat[i].length; j++) {
+                if(mat[i][j] == 1 && row[i] == 1 && col[j] == 1) count++;
+            }
+        }
         
-        
-        
-        
+        return count; 
     }
 }
